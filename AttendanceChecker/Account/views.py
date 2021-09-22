1e0740e2ce053051316f7e0ee3cf8c97b9b1532b
Colby0506 <Colby0506@hotmail.com>
2021-09-22 14:53:01 -0400

    UpdateToSignin

--- AttendanceChecker/Account/views.py
+++ AttendanceChecker/Account/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
 # Create your views here.
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+def SignUpView(request):
+    if request.method == "POST":
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+            return redirect("/")
+    else:
+        form = CustomUserCreationForm()
+        return render(request, "registration/signup.html", {"form":form})
