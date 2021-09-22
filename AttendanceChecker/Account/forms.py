7d9e2c18d548e65005e478fc37b6d473ccc0c916
Colby0506 <Colby0506@hotmail.com>
2021-09-22 16:35:47 -0400

    Added  account log in top left

--- AttendanceChecker/Account/forms.py
+++ AttendanceChecker/Account/forms.py
@@ -5,10 +5,14 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = {"first_name","last_name",'username','email','is_teacher','is_student'}
+        fields = {"first_name","last_name",'username','email','is_teacher'}
         def save(self, commit = True):
             user = super(CustomUserCreationForm, self).save(commit = False)
             user.email = self.cleaned_data['email']
+            if user.is_teacher == True:
+                user.is_student = False
+            else:
+                user.is_student = True
             if commit:
                 user.save()
             return user
